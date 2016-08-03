434a1adab590ecb75e5f3a54b133b0c7f7b48a7a
applqpak <applqpak@gmail.com>
2016-08-03 18:00:22 +1000

    Create Main.java

---
+++ applqpak/HelpModifier/Main.java
@@ -0,0 +1,67 @@
+
+package applqpak.HelpModifier;
+
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
+import cn.nukkit.utils.TextFormat;
+
+import java.util.*;
+import java.io.File;
+
+public class Main extends PluginBase
+{
+
+  public Config config;
+
+  public String[] toS(List lst)
+  {
+
+    String[] strArray = new String[lst.size()];
+
+    int index = 0;
+
+    for(Object value : lst)
+    {
+
+      strArray[index] = String.valueOf(value);
+
+      index++;
+
+    }
+
+    return strArray;
+
+  }
+
+  @Override
+
+  public void onEnable()
+  {
+
+    this.getDataFolder().mkdirs();
+
+    if(!(new File(this.getDataFolder(), "config.yml").exists()))
+    {
+
+      saveResource("config.yml");
+
+    }
+
+    this.config = getConfig();
+
+    this.getServer().getPluginManager().registerEvents(new EventListener(this), this);
+
+    this.getLogger().info(TextFormat.GREEN + "Enabled.");
+
+  }
+
+  @Override
+
+  public void onDisable()
+  {
+
+    this.getLogger().info(TextFormat.RED + "Disabled.");
+
+  }
+
+}
